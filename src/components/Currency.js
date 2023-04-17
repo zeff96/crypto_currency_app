e13e9112873fcc87c68cdbd09a2f448ef1b41ba5
zeff96 <zeffo1975@gmail.com>
2023-04-17 14:23:02 +0300

    Change to display grid for the categories

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -4,16 +4,16 @@ import { BsArrowRightCircle } from "react-icons/bs";
 
 export default function Currency({ coin }) {
   return (
-    <div className="card p-3 mb-3">
-      <div className="row">
-        <div className="col-6">
+    <div className="card p-3 mb-3 ms-3 col-sm-5 col-lg-3">
+      <div className="d-flex">
+        <div className="col">
           <img className="img-fluid" src={coin.img} alt={coin.name} />
         </div>
-        <div className="col-6 d-flex flex-column align-items-end justify-content-between">
+        <div className="col d-flex flex-column align-items-end justify-content-between">
           <NavLink to={`/details/${coin.id}`} className="text-white fs-2">
             <BsArrowRightCircle />
           </NavLink>
-          <h2>{coin.name}</h2>
+          <p className="f-4">{coin.name}</p>
         </div>
       </div>
     </div>
