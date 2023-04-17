1b219d3c6af6274bf5b9d3b6ebb0b92f9fb79bcd
zeff96 <zeffo1975@gmail.com>
2023-04-17 11:36:41 +0300

    Add navbar items.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,27 @@
 import { NavLink } from "react-router-dom";
 
+import { BsChevronLeft } from "react-icons/bs";
+import { MdOutlineKeyboardVoice, MdSettings } from "react-icons/md";
+
 export default function Header() {
   return (
     <nav className="navbar">
       <div className="container">
-        <NavLink to="/" className="navbar-brand"></NavLink>
+        <NavLink to="/" className="navbar-brand">
+          <BsChevronLeft />
+        </NavLink>
         <div className="collapse navbar-collpase">
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item"></li>
-            <li className="nav-item"></li>
+            <li className="nav-item">
+              <NavLink className="nav-link">
+                <MdOutlineKeyboardVoice />
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link">
+                <MdSettings />
+              </NavLink>
+            </li>
           </ul>
         </div>
       </div>
