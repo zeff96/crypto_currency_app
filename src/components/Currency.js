848ac50ba7d0506368c55fd2ff40e6b376e79fb8
zeff96 <zeffo1975@gmail.com>
2023-04-17 13:43:43 +0300

    Add display flex to cards and padding

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -4,13 +4,13 @@ import { BsArrowRightCircle } from "react-icons/bs";
 
 export default function Currency({ coin }) {
   return (
-    <div className="card p-2 mb-3">
+    <div className="card p-3 mb-3">
       <div className="row">
         <div className="col-6">
           <img className="img-fluid" src={coin.img} alt={coin.name} />
         </div>
-        <div className="col-6">
-          <NavLink to={`/details/${coin.id}`}>
+        <div className="col-6 d-flex flex-column align-items-end justify-content-between">
+          <NavLink to={`/details/${coin.id}`} className="text-white fs-2">
             <BsArrowRightCircle />
           </NavLink>
           <h2>{coin.name}</h2>
