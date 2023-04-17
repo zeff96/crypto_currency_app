dea3b11e783d5c0fa3978619711c83b3e452f49f
zeff96 <zeffo1975@gmail.com>
2023-04-17 12:31:31 +0300

    Add coin categories data

---
+++ src/utilis/allCoins.js
@@ -0,0 +1,35 @@
+import bitcoin from "../asset/bitcoin.svg";
+import binancecoin from "../asset/binance-coin-seeklogo.com.svg";
+import binanceusd from "../asset/binance-usd-busd-seeklogo.com.svg";
+import cardano from "../asset/cardano-ada-seeklogo.com.svg";
+import dogecoin from "../asset/dogecoin-seeklogo.com.svg";
+import ethereum from "../asset/ethereum-seeklogo.com.svg";
+import litecoin from "../asset/litecoin-seeklogo.com.svg";
+import polkadot from "../asset/polkadot-dot-seeklogo.com.svg";
+import polygon from "../asset/polygon-studios-seeklogo.com.svg";
+import ripple from "../asset/ripple-xrp-seeklogo.com.svg";
+import shiba from "../asset/shiba-inu-shib-seeklogo.com.svg";
+import solana from "../asset/solana-sol-seeklogo.com.svg";
+import tether from "../asset/tether-seeklogo.com.svg";
+import tron from "../asset/tron-seeklogo.com.svg";
+import usd from "../asset/usd-coin-usdc-seeklogo.com.svg";
+import avalanche from "../asset/colorado-avalanche.svg";
+
+export const allCoins = [
+  { id: "bitcoin", name: "Bitcoin", img: bitcoin },
+  { id: "binance-coin", name: "BNB", img: binancecoin },
+  { id: "binance-usd", name: "Binance USD", img: binanceusd },
+  { id: "cardano", name: "Cardano", img: cardano },
+  { id: "dogecoin", name: "Dogecoin", img: dogecoin },
+  { id: "ethereum", name: "Ethereum", img: ethereum },
+  { id: "litecoin", name: "Litecoin", img: litecoin },
+  { id: "polkadot", name: "Polkadot", img: polkadot },
+  { id: "polygon", name: "Pylogon", img: polygon },
+  { id: "xrp", name: "XRP", img: ripple },
+  { id: "shiba-inu", name: "Shiba Inu", img: shiba },
+  { id: "solana", name: "Solana", img: solana },
+  { id: "tether", name: "Tether", img: tether },
+  { id: "tron", name: "TRON", img: tron },
+  { id: "usd-coin", name: "USD Coin", img: usd },
+  { id: "avalanche", name: "Avalanche", img: avalanche },
+];
