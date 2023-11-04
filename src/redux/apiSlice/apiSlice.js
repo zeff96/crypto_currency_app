4e0492c9721b5ca3f9de2891b5adacfbe5a0f348
zeff96 <zeffo1975@gmail.com>
2023-11-04 21:52:46 +0300

    Add baseQuery

---
+++ src/redux/apiSlice/apiSlice.js
@@ -0,0 +1,6 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+const apiSlice = createApi({
+    baseQuery: fetchBaseQuery({baseUrl: 'https://api.coingecko.com/api/v3/coins/'}),
+    endpoints: () => ({})
+})
