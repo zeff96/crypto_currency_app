5d8f261281d9f805cea5a2cadf1ad09a8e8aa457
zeff96 <zeffo1975@gmail.com>
2023-11-04 22:02:23 +0300

    Add store wrapper to app top level component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import setupStore from './redux/store';
+import store from './redux/store';
 import App from './App';
-
-const store = setupStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
