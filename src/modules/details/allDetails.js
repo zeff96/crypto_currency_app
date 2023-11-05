ce9c2e5ed0146fb46228580887b03f685906a19c
zeff96 <zeffo1975@gmail.com>
2023-11-05 18:28:52 +0300

    Update details page to use data from store

--- src/modules/details/allDetails.js
+++ src/modules/details/allDetails.js
@@ -1,35 +1,41 @@
-// import { useParams } from 'react-router-dom';
-// import useGetCoinsQuery from '../../redux/coins/coinsSlice';
-// import Details from './details';
-// import Header, { Detailspagenav } from '../navbar/header';
-// import logo from '../../asset/bitcoin.svg';
+import { useParams } from 'react-router-dom';
+import useGetCoinsQuery from '../../redux/coins/coinsSlice';
+import Details from './details';
+import Header, { Detailspagenav } from '../navbar/header';
+import logo from '../../asset/bitcoin.svg';
 
-// export default function Alldetails() {
-//   const { id } = useParams();
+export default function Alldetails() {
+  const { id } = useParams();
 
-//   return (
-//     <div className="container my-5 pt-5">
-//       <Header>
-//         <Detailspagenav />
-//       </Header>
-//       <div className="mb-3 d-flex align-items-center">
-//         <div>
-//           <img src={logo} alt="binance-svg" style={{ width: '15rem' }} />
-//         </div>
-//         <div>
-//           <span className="text-white fs-2">
-//             CRYPTO
-//             <br />
-//             CURRENCY
-//             <br />
-//             DATA
-//           </span>
-//         </div>
-//       </div>
-//       <hr className="text-white" />
-//       {status === 'loading' && <h3>Loading...</h3>}
-//       {error && <h3>{error.message}</h3>}
-//       {detail && <Details key={detail.id} detail={detail} />}
-//     </div>
-//   );
-// }
+  const {
+    data = [], isError, isLoading, isSuccess, error,
+  } = useGetCoinsQuery();
+
+  const detail = data.find((coin) => coin?.id === id);
+
+  return (
+    <div className="container my-5 pt-5">
+      <Header>
+        <Detailspagenav />
+      </Header>
+      <div className="mb-3 d-flex align-items-center">
+        <div>
+          <img src={logo} alt="binance-svg" style={{ width: '15rem' }} />
+        </div>
+        <div>
+          <span className="text-white fs-2">
+            CRYPTO
+            <br />
+            CURRENCY
+            <br />
+            DATA
+          </span>
+        </div>
+      </div>
+      <hr className="text-white" />
+      {isLoading && <h3>Loading...</h3>}
+      {isError && <h3>{error.message}</h3>}
+      {isSuccess && <Details key={detail?.id} detail={detail} />}
+    </div>
+  );
+}
